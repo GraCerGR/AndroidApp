aab3b3142e553f59cf3b509d88fd6009f0b9fc12
GraCerGR <ctrhtnysq.afqk@gmail.com>
2023-05-10 23:50:53 +0700

    Появление блока присваивания при нажатии

--- app/src/main/java/com/example/myapplication/Main.kt
+++ app/src/main/java/com/example/myapplication/Main.kt
@@ -1,32 +1,27 @@
 package com.example.myapplication
 
-import android.content.ClipData
-import android.content.ClipDescription
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MotionEvent
-import android.view.View
+import android.view.LayoutInflater
 import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import com.google.android.material.bottomsheet.BottomSheetDialog
+import android.widget.FrameLayout
 
 class Main : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       /* val bottomSheetFragment = BottomSheetFragment()
+        /* val bottomSheetFragment = BottomSheetFragment()
         bottomSheetFragment.*/
 
         val bottomSheetFragment = BottomSheetFragment()
-/*        var button1 = findViewById<Button>(R.id.button1)
+        /*        var button1 = findViewById<Button>(R.id.button1)
         button1.setOnClickListener{
             bottomSheetFragment.show(supportFragmentManager,"Bottom")
         }*/
 
-       /* BottomSheetFragment.setOnClickListener {
+        /* BottomSheetFragment.setOnClickListener {
 
             bottomSheetFragment.show(supportFragmentManager,"Bottom")
             *//*val setIntent = Intent(this, BottomSheetFragment::class.java)
@@ -34,8 +29,7 @@ class Main : AppCompatActivity() {
         }*/
 
 
-
-        var assignment = findViewById<TextView>(R.id.Assignment)
+        /*        var assignment = findViewById<TextView>(R.id.Assignment)
         assignment.setOnTouchListener { v, _ ->
             val item = ClipData.Item(assignment.text)
 
@@ -61,8 +55,33 @@ class Main : AppCompatActivity() {
             // Indicate that the long-click is handled.
             true
 
-        }
+        }*/
+        /*var varBlock = findViewById<>(R.id.VarBlock)*/
+
+        /*val rootLayout: RelativeLayout = findViewById(R.id.drawerLayout)*/
+        val secondActivityContainer: FrameLayout = findViewById(R.id.secondActivityContainer)
+
+        var assignment = findViewById<Button>(R.id.Assignment)
+        val navigationView = findViewById<com.google.android.material.navigation.NavigationView>(R.id.navigationView)
+        /*var layout = findViewById<LinearLayout>(R.id.container)*/
+        assignment.setOnClickListener {
+            /*val rootView: ViewGroup = findViewById(android.R.id.content)
+            val inflater: LayoutInflater = LayoutInflater.from(this)
+
+            val layout = inflater.inflate(R.layout.item_assignment1, rootView, false)
+            rootView.addView(layout)*/
+
+            val inflater: LayoutInflater = LayoutInflater.from(this)
+            val layout = inflater.inflate(R.layout.assignment_block1, secondActivityContainer, false)
+            secondActivityContainer.addView(layout)
+
+            val drawerLayout = findViewById<androidx.drawerlayout.widget.DrawerLayout>(R.id.drawerLayout) // Получение ссылки на DrawerLayout
+            drawerLayout.closeDrawer(navigationView) // Закрытие NavigationView
 
         }
-
     }
+}
+
+
+
+
