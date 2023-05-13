4736611ba36d8d418025153e4f1d9c20a93b4248
GraCerGR <ctrhtnysq.afqk@gmail.com>
2023-05-13 19:30:10 +0700

    Сделал блоки Определения и Присваивания с одинаковыми свойствами(UniversalAdapter)

--- app/src/main/java/com/example/myapplication/Main.kt
+++ app/src/main/java/com/example/myapplication/Main.kt
@@ -16,7 +16,7 @@ import androidx.recyclerview.widget.RecyclerView.ViewHolder
 class Main : AppCompatActivity() {
 
     private lateinit var container: RecyclerView
-    private lateinit var adapter: AssignmentAdapter
+    private lateinit var adapter: UniversalAdapter
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,16 +25,26 @@ class Main : AppCompatActivity() {
 
         container = findViewById<RecyclerView>(R.id.DefinitionRecyclerView)
         container.layoutManager = LinearLayoutManager(this)
-        adapter = AssignmentAdapter()
+
+        adapter = UniversalAdapter(R.layout.definition_block1) // Создание экземпляра адаптера
         container.adapter = adapter
         val navigationView = findViewById<com.google.android.material.navigation.NavigationView>(R.id.navigationView)
+        val drawerLayout = findViewById<androidx.drawerlayout.widget.DrawerLayout>(R.id.drawerLayout) // Получение ссылки на DrawerLayout
 
-        val assignmentButton = findViewById<Button>(R.id.Definition)
+        val assignmentButton = findViewById<Button>(R.id.Assignment)
+        val definitionButton = findViewById<Button>(R.id.Definition)
 
         assignmentButton.setOnClickListener {
-            // Добавление нового элемента в список
-            adapter.addAssignment()
-            val drawerLayout = findViewById<androidx.drawerlayout.widget.DrawerLayout>(R.id.drawerLayout) // Получение ссылки на DrawerLayout
+            // Устанавливаем layoutId для адаптера как R.layout.assignment_block1
+            adapter.setLayoutId(R.layout.assignment_block1)
+            adapter.addItem()
+            drawerLayout.closeDrawer(navigationView) // Закрытие NavigationView
+        }
+
+        definitionButton.setOnClickListener {
+            // Устанавливаем layoutId для адаптера как R.layout.definition_block1
+            adapter.setLayoutId(R.layout.definition_block1)
+            adapter.addItem()
             drawerLayout.closeDrawer(navigationView) // Закрытие NavigationView
         }
 
@@ -44,51 +54,54 @@ class Main : AppCompatActivity() {
         }
         val itemTouchHelper = ItemTouchHelper(SimpleItemTouchHelperCallback(adapter))
         itemTouchHelper.attachToRecyclerView(container)
-
     }
-
 }
 
 // Создайте класс Adapter для RecyclerView
-class AssignmentAdapter : RecyclerView.Adapter<AssignmentAdapter.AssignmentViewHolder>(),
+class UniversalAdapter(private var layoutId: Int) : RecyclerView.Adapter<UniversalAdapter.ViewHolder>(),
     ItemTouchHelperAdapter {
 
-    private val assignments = mutableListOf<Any>()
+    private val items = mutableListOf<Any>()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AssignmentViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val itemView = inflater.inflate(R.layout.definition_block1, parent, false)
-        return AssignmentViewHolder(itemView)
+        val itemView = inflater.inflate(layoutId, parent, false)
+        return ViewHolder(itemView)
     }
 
-    override fun onBindViewHolder(holder: AssignmentViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         // Здесь вы можете настроить отображение данных в каждом элементе списка
     }
 
     override fun getItemCount(): Int {
-        return assignments.size
+        return items.size
     }
 
-    fun addAssignment() {
+    fun addItem() {
         // Добавление нового элемента в список
-        assignments.add(Any()) // Здесь вы можете использовать свои собственные данные
-        notifyItemInserted(assignments.size - 1)
+        items.add(Any()) // Здесь вы можете использовать свои собственные данные
+        notifyItemInserted(items.size - 1)
     }
 
     override fun onItemMove(fromPosition: Int, toPosition: Int) {
         // Перемещение элемента в списке
-        val assignment = assignments.removeAt(fromPosition)
-        assignments.add(toPosition, assignment)
+        val item = items.removeAt(fromPosition)
+        items.add(toPosition, item)
         notifyItemMoved(fromPosition, toPosition)
     }
 
     override fun onItemDismiss(position: Int) {
         // Удаление элемента из списка
-        assignments.removeAt(position)
+        items.removeAt(position)
         notifyItemRemoved(position)
     }
 
-    class AssignmentViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    fun setLayoutId(newLayoutId: Int) {
+        layoutId = newLayoutId
+        notifyDataSetChanged()
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 }
 
 class SimpleItemTouchHelperCallback(adapter: ItemTouchHelperAdapter) :
